7629f95154eea2c6464674554ac95f8d20362054
tjsgh5965 <tjsgh5965@gmail.com>
2026-02-22 11:36:54 +0900

    fix: 4강 강의 수정 - watch 대신 computed를 활용해서 소스를 간소화하는 방법 학습 - watch가 언제 필요한지 예시를 통해 학습

--- 4. 양방향 바인딩/app.js
+++ 4. 양방향 바인딩/app.js
@@ -4,41 +4,48 @@ const app = Vue.createApp({
       counter: 0,
       name: '',
       lastName: '',
-      fullName: '',
+      // fullName: '',
     };
   },
   watch: {
-    // 해당 메소드는 name이 변경될 때마다 자동으로 실행됨.
-    name(value) {
-      if (value === '') {
-        this.fullName = '';
-        return;
+    counter(value) {
+      if (value > 50) {
+        setTimeout(() => {
+          this.counter = 0;
+        }, 500);
       }
-      // name이 변경될 때마다 그때그때 변경된 최신값을 value로 던지기 때문에 굳이 this.name이라 쓸 필요 없음
-      // this.fullName = this.name + ' ' + 'Test001';
-      this.fullName = value + ' ' + this.lastName;
-    },
-    // 인자를 2개 받을 수도 있음.
-    // name(newValue, oldValue) {
-    //   console.log('New Value: ' + newValue);
-    //   console.log('Old Value: ' + oldValue);
-    // },
-    lastName(value) {
-      if (value === '') {
-        this.fullName = '';
-        return;
-      }
-      this.fullName = this.name + ' ' + value;
     }
+    // 해당 메소드는 name이 변경될 때마다 자동으로 실행됨.
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //     return;
+    //   }
+    //   // name이 변경될 때마다 그때그때 변경된 최신값을 value로 던지기 때문에 굳이 this.name이라 쓸 필요 없음
+    //   // this.fullName = this.name + ' ' + 'Test001';
+    //   this.fullName = value + ' ' + this.lastName;
+    // },
+    // // 인자를 2개 받을 수도 있음.
+    // // name(newValue, oldValue) {
+    // //   console.log('New Value: ' + newValue);
+    // //   console.log('Old Value: ' + oldValue);
+    // // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //     return;
+    //   }
+    //   this.fullName = this.name + ' ' + value;
+    // }
   },
   computed: { // 메소드가 아니라 데이터(속성)으로 취급되므로 HTML에서 사용 시 괄호를 붙이지 않음
-    // fullName() { // 캐싱된 결과를 반환하는 계산된 속성 (Computed Property)
-    //   console.log('Running again...');
-    //   if (this.name === '') {
-    //     return '';
-    //   }
-    //   return this.name + ' ' + 'Test001';
-    // }
+    fullName() { // 캐싱된 결과를 반환하는 계산된 속성 (Computed Property)
+      console.log('Running again...');
+      if (this.name === '' || this.lastName === '') {
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    }
   },
   // 그럼 대체 메소드를 언제 쓰느냐?
   // computed는 데이터 기반 "계산 값"이라는 것을 인지해야 한다.
