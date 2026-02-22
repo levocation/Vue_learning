e2a9e5d5c3c51f9f849be1242f2bcd58b1477f89
tjsgh5965 <tjsgh5965@gmail.com>
2026-02-22 09:41:09 +0900

    fix: 4강 과제 수정 - outputFullName method 추가 - outputFullName에 console.log 추가하여 Vue에서 데이터 변경 시 템플릿을 재렌더링하는 것을 확인

--- 4. 양방향 바인딩/app.js
+++ 4. 양방향 바인딩/app.js
@@ -6,6 +6,14 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    outputFullName() {
+      // Vue의 특성상 데이터가 변경되면 Template 전체를 재렌더링하기 때문에, 이 메서드가 실행될 때마다 콘솔에 'Running again...'이 출력됨.
+      console.log('Running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Test001';
+    },
     setName(event, lastName) {
       this.name = event.target.value;
     },
